8af72cdf63a69d28d201c2e072d4987df5b053b2
emisystems <105679761+emisystems@users.noreply.github.com>
2022-07-08 22:19:57 -0500

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -1,7 +1,7 @@
 const sonarqubeScanner = require('sonarqube-scanner');
 
 sonarqubeScanner({
-  serverUrl: 'http:54.161.76.202:9000/',
+  serverUrl: 'http://54.161.76.202:9000/',
        options : {
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
